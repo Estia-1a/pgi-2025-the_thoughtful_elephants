93743f09ae5006ddbe16c49ea9e28fc4940d3827
Chris-ira <christophe.irastorza@etu.estia.fr>
2025-05-26 10:51:12 +0200

    presque fini

--- src/main.c
+++ src/main.c
@@ -38,22 +38,21 @@ int main(int argc, char **argv) {
   }
 
   if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
-    /* dimension() function is defined in feature.h and implemented in feature.c */
+    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel( configuration.filenames[0] );
   }
 
   if ( strncmp( configuration.command, "tenth_pixel", 11 ) == 0 ) {
-    /* dimension() function is defined in feature.h and implemented in feature.c */
+    /* tenth_pixel() function is defined in feature.h and implemented in feature.c */
     tenth_pixel( configuration.filenames[0] );
   }
 
   if ( strncmp( configuration.command, "second_line", 11 ) == 0 ) {
-    /* dimension() function is defined in feature.h and implemented in feature.c */
+    /* second_line() function is defined in feature.h and implemented in feature.c */
     second_line( configuration.filenames[0] );
   }
-  /*
-   * TO COMPLETE
-   */
-  
+  if (strncmp(configuration.command, "print_pixel",11)==0) {
+    print_pixel(configuration.filenames[0]);
+  }
   return 0;
 }
