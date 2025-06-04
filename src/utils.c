a22e40a5b0dc5e7a6898a0df295b21900333a6c3
Chris-ira <christophe.irastorza@etu.estia.fr>
2025-06-04 13:56:08 +0200

    correction doublons 2

--- src/utils.c
+++ src/utils.c
@@ -3,15 +3,6 @@
 #include "utils.h"
 
 
-pixelRGB * get_pixel( unsigned char *data , unsigned int width , unsigned int height , const unsigned int n , const unsigned int x , const unsigned int y ) {
-    if ( x > width || y > height || *data == '\0' ) {
-        return NULL;
-    }
-    else {
-        return (pixelRGB *) &data[ x * y * 3 - 1 ] ;
-    }
-}
-
 pixelRGB *get_pixel (unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y){
     
     if (!data || x >= width || y >= height){
